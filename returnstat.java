40bb78ce9658890404eaf100d93e2f5b44516f40
Oyewole Oyediran <woleoyediran@gmail.com>
2025-09-04 13:41:16 +0100

    fifth commit

---
+++ returnstat.java
@@ -0,0 +1,9 @@
+public class returnstat {
+    public static void main(String[] args) {
+        cube(4);
+        System.out.println(cube(4));
+    }
+    public static int cube(int x) {
+        return (x * x * x);
+    }
+}
