4c73c146f3e90368c146003103d056190fdbb148
Oyewole Oyediran <woleoyediran@gmail.com>
2025-09-04 13:26:27 +0100

    fourth commit

---
+++ methods.java
@@ -0,0 +1,12 @@
+public  class methods {
+    public static void main(String[] args) {
+        sayHi("Wholes", 90);
+       
+    }
+
+    
+
+     public static void sayHi(String name, int age) {
+        System.out.println("Hello " + name + " you are " + age);
+     }
+}
