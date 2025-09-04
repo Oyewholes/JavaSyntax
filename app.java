d70042ebe32efcbc6b03b120b40a90453ed1caf7
Oyewole Oyediran <woleoyediran@gmail.com>
2025-09-04 11:28:49 +0100

    second java commit

--- app.java
+++ app.java
@@ -37,28 +37,48 @@ public class app {
 		// double num2 = keyboardInput.nextDouble();
 		// System.out.println(num2 + num1);
 
+        // Scanner input = new Scanner(System.in);
+        // int number1;
+        // int number2;
+        // int sum;
+        // int product;
+        // int difference;
+        // int division;
+
+        // System.out.print("Enter first integer");
+        // number1 = input.nextInt();
+
+        // System.out.print("Enter second integer");
+        // number2 = input.nextInt();
+
+        // sum = number1 + number2;
+        // product = number1 * number2;
+        // difference = number1 - number2;
+        // division = number1/number2;
+        // System.out.printf("Sum is %d%n", sum);
+        // System.out.printf("Product is %d%n", product);
+        // System.out.printf("Difference is %d%n", difference);
+        // System.out.printf("Division is %d%n", division);
+
         Scanner input = new Scanner(System.in);
         int number1;
         int number2;
-        int sum;
-        int product;
-        int difference;
-        int division;
-
-        System.out.print("Enter first integer");
+        System.out.println("Enter first integer: ");
         number1 = input.nextInt();
-
-        System.out.print("Enter second integer");
+        System.out.println("Enter second integer: ");
         number2 = input.nextInt();
-
-        sum = number1 + number2;
-        product = number1 * number2;
-        difference = number1 - number2;
-        division = number1/number2;
-        System.out.printf("Sum is %d%n", sum);
-        System.out.printf("Product is %d%n", product);
-        System.out.printf("Difference is %d%n", difference);
-        System.out.printf("Division is %d%n", division);
+        if (number1 == number2)
+            System.out.printf("%d == %d%n", number1, number2);
+        if (number1 != number2)
+            System.out.printf("%d != %d%n", number1, number2);
+        if (number1 < number2)
+            System.out.printf("%d < %d%n", number1, number2);
+        if (number1 > number2)
+            System.out.printf("%d > %d%n", number1, number2);
+        if (number1 <= number2)
+            System.out.printf("%d <= %d%n", number1, number2);
+        if (number1 >= number2)
+            System.out.printf("%d >= %d%n", number1, number2);
 	}
 
 }
